06a20199179572e6f89606dfbff62cda1c5a225c
Sirajul Arfin <78791175+sirajularfin@users.noreply.github.com>
2025-11-06 12:07:23 +0530

    fix button style

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -3,16 +3,14 @@ import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
   pressable: {
-    overflow: 'hidden',
     borderRadius: ScaledSize.SIZE_8,
+    paddingVertical: ScaledSize.SIZE_12,
   },
   container: {
     alignItems: 'center',
     justifyContent: 'center',
 
     minHeight: ScaledSize.SIZE_48,
-    paddingVertical: ScaledSize.SIZE_12,
-    paddingHorizontal: ScaledSize.SIZE_24,
     borderRadius: ScaledSize.SIZE_8,
   },
 });
