42504adab01a8cb989976f2f9ded27554130b0f9
Sirajul Arfin <78791175+sirajularfin@users.noreply.github.com>
2025-11-06 12:11:13 +0530

    feat: add bottom tab animation on navigation

---
+++ src/screens/ManageCurrencies/styles.ts
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native';
+
+import { ScaledSize } from '@/src/common/theme/sizes';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginHorizontal: ScaledSize.SIZE_20,
+  },
+});
+
+export default styles;
