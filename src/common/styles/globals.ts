8a15778d33254d648fdafff4d9272f7d5b847798
Sirajul Arfin <78791175+sirajularfin@users.noreply.github.com>
2025-11-06 13:46:44 +0530

    use common style in bottom tab

--- src/common/styles/globals.ts
+++ src/common/styles/globals.ts
@@ -5,7 +5,7 @@ import { ScaledSize } from '../theme/sizes';
 const globalStyles = StyleSheet.create({
   shadow: {
     shadowColor: Colors.BLACK,
-    shadowOffset: { width: ScaledSize.ZERO, height: ScaledSize.SIZE_1 },
+    shadowOffset: { width: ScaledSize.ZERO, height: ScaledSize.SIZE_2 },
     shadowOpacity: 0.25,
     shadowRadius: ScaledSize.SIZE_2,
     elevation: ScaledSize.SIZE_2, // Shadow for Android
--- src/navigation/components/styles.ts
+++ src/navigation/components/styles.ts
@@ -1,5 +1,6 @@
 import { StyleSheet } from 'react-native';
 
+import globalStyles from '@/src/common/styles/globals';
 import { Colors } from '@/src/common/theme/colors';
 import { FONT_TYPE_REGULAR, getFont } from '@/src/common/theme/fonts';
 import { ScaledSize, Size } from '@/src/common/theme/sizes';
@@ -14,19 +15,12 @@ const styles = StyleSheet.create({
     borderTopWidth: Size.ZERO,
   },
   tabBar: {
+    ...globalStyles.shadow,
     backgroundColor: Colors.TRANSPARENT,
     borderRadius: ScaledSize.SIZE_8,
     height: ScaledSize.SIZE_70,
     paddingTop: ScaledSize.SIZE_8,
     marginHorizontal: ScaledSize.SIZE_15,
-    shadowColor: Colors.BLACK,
-    shadowOffset: {
-      width: ScaledSize.ZERO,
-      height: ScaledSize.SIZE_2,
-    },
-    elevation: ScaledSize.SIZE_2,
-    shadowOpacity: 0.25,
-    shadowRadius: Size.SIZE_2,
   },
   tabBarLabel: {
     fontFamily: getFont(FONT_TYPE_REGULAR),
