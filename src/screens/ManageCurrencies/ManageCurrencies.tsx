0aac3a6cf3b31ecd39eae10d228661dbab71308f
Sirajul Arfin <78791175+sirajularfin@users.noreply.github.com>
2025-11-10 12:46:01 +0530

    feat: update manage currencies screen

--- src/screens/ManageCurrencies/ManageCurrencies.tsx
+++ src/screens/ManageCurrencies/ManageCurrencies.tsx
@@ -1,15 +1,71 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useState } from 'react';
+import { FlatList, View } from 'react-native';
 
 import MainLayout from '@/src/common/layouts/MainLayout';
+import { Colors } from '@/src/common/theme/colors';
+import { ScaledSize } from '@/src/common/theme/sizes';
+import { SUPPORTED_CURRENCIES } from '@/src/common/types/constants';
+import { ICurrencyInfo } from '@/src/common/types/currency.type';
+import Button from '@/src/components/Button/Button';
+import CurrencyCard from '@/src/components/CurrencyCard/CurrencyCard';
+import CurrencyInput from '@/src/components/CurrencyInput/CurrencyInput';
+import Typography, { Variant } from '@/src/components/Typography/Typography';
+import { useCurrencyRate } from '@/src/contexts/CurrencyRate/CurrencyRateContext';
 import { API_ROUTES, RootNavigationProps } from '@/src/navigation/types';
+import styles from './styles';
+
+const itemSeparator = () => <View style={{ height: ScaledSize.SIZE_20 }} />;
 
 const ManageCurrenciesScreen: React.FC<
   RootNavigationProps<API_ROUTES.MANAGE_CURRENCIES>
 > = () => {
+  const [selectedCurrency, setSelectedCurrency] =
+    useState<ICurrencyInfo | null>(null);
+
+  const { state, functions } = useCurrencyRate();
+
   return (
-    <MainLayout>
-      <Text>Manage Currencies Screen</Text>
+    <MainLayout barStyle="dark-content" backgroundColor={Colors.GREY_50}>
+      <View style={styles.container}>
+        <Typography variant={Variant.headingSmall} color={Colors.GREY_900}>
+          Manage Rates
+        </Typography>
+        <CurrencyInput
+          showHint
+          showEnteredValue
+          onCurrencyChange={setSelectedCurrency}
+          placeholder="Enter rate (e.g. 100)"
+          dropdownData={SUPPORTED_CURRENCIES}
+        />
+        <Button
+          onPress={() => {
+            selectedCurrency && functions.addCurrency(selectedCurrency);
+          }}
+          disabled={!selectedCurrency}
+        >
+          Add Rate
+        </Button>
+        <View style={styles.divider} />
+        <Typography variant={Variant.titleMedium} color={Colors.GREY_900}>
+          Stored currencies
+        </Typography>
+        <FlatList
+          data={state.currencies}
+          keyExtractor={item => item.code}
+          renderItem={({ item }) => (
+            <CurrencyCard
+              amount={item.currentRate?.toString() ?? ''}
+              currencyCode={item.code}
+              currencyName={item.name}
+              countryName={item.origin}
+              flagUri={item.flag}
+              onDelete={() => functions.removeCurrency(item.code)}
+            />
+          )}
+          ItemSeparatorComponent={itemSeparator}
+          ListEmptyComponent={null}
+        />
+      </View>
     </MainLayout>
   );
 };
--- src/screens/ManageCurrencies/styles.ts
+++ src/screens/ManageCurrencies/styles.ts
@@ -1,12 +1,16 @@
 import { StyleSheet } from 'react-native';
 
+import globalStyles from '@/src/common/styles/globals';
 import { ScaledSize } from '@/src/common/theme/sizes';
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    marginHorizontal: ScaledSize.SIZE_20,
+    ...globalStyles.flexContainer,
+    gap: ScaledSize.SIZE_20,
   },
-});
+  divider: {
+    height: ScaledSize.SIZE_20,
+  },
+})
 
 export default styles;
